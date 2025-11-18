660fcec0d8ca9c1070a009c2bb9d002bcfbc55d7
Ahmed <ae9474510@gmail.com>
2025-11-18 19:45:53 +0200

    merge from  feature/orders-page into dev

--- src/components/features/product-card/product-card.tsx
+++ src/components/features/product-card/product-card.tsx
@@ -13,7 +13,7 @@ import { useTranslations } from 'next-intl';
 import { Button } from '@/components/ui/button';
 import WishlistButton from '../wishlist/wishlist-button';
 
-
+// props
 type ProductCardProps = {
   product: Product;
 };
