f50093d643885752caa7476ca2a6c2013442e2ad
Moataz <moatazlevi@gmail.com>
2025-12-05 18:42:25 +0200

    fix(cart): fix cart api used in summary section

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,20 +1,13 @@
+import FirstRow from "./_components/first-row";
+
 // src/app/[locale]/dashboard/page.tsx
-export default function Page() {
+export default function Page({ params }: { params: { locale: string } }) {
+  const { locale } = params;
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Overview</h1>
+    <div >
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="p-6 bg-white rounded-xl shadow">
-          <h2 className="text-xl font-semibold mb-2">Stats</h2>
-          <p>Dashboard main content goes here...</p>
-        </div>
+      <FirstRow locale={locale} />
 
-        <div className="p-6 bg-white rounded-xl shadow">
-          <h2 className="text-xl font-semibold mb-2">Orders</h2>
-          <p>Latest orders or analytics...</p>
-        </div>
-      </div>
     </div>
   );
 }
