ee554abc3382aba2e3efea45bb0463c2074a134e
Moataz <moatazlevi@gmail.com>
2026-03-12 23:08:22 +0200

    chore(user-profile): fix user profile in dashboard

--- src/app/[locale]/dashboard/(profile)/account-settings/_components/account-settings.tsx
+++ src/app/[locale]/dashboard/(profile)/account-settings/_components/account-settings.tsx
@@ -93,7 +93,7 @@ export default function ProfileAccount({ userData }: MyAccountProps) {
   // Functions
   // Submit handler for saving profile changes
   const onSubmit: SubmitHandler<profileValues> = (value) => {
-    const { ...payload } = value;
+    const { gender, ...payload } = value;
     editProfile(payload);
   };
 
